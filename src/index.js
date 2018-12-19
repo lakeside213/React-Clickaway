881a99bd358ca00ecb33a4c1b11f53c380ce8602
lekan <kokulekan23@gmail.com>
2018-12-19 20:00:10 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import { render } from "react-dom";
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+class SampleComponent extends Component {
+  state = {
+    clickedOutside: false
+  };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  myRef = React.createRef();
+
+  handleClickOutside = e => {
+    if (!this.myRef.current.contains(e.target)) {
+      this.setState({ clickedOutside: true });
+    }
+  };
+
+  handleClickInside = () => this.setState({ clickedOutside: false });
+
+  render() {
+    return (
+      <button ref={this.myRef} onClick={this.handleClickInside}>
+        {this.state.clickedOutside ? "Bye!" : "Hello!"}
+      </button>
+    );
+  }
+}
+
+const App = () => (
+  <div style={styles}>
+    <SampleComponent />
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
+
+export default SampleComponent;
