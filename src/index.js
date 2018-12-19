2fe52ea25343e096c6686a49ecd20502ad929c5f
lekan <kokulekan23@gmail.com>
2018-12-19 20:17:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,57 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
-const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center"
-};
-class SampleComponent extends Component {
-  state = {
-    clickedOutside: false
-  };
+import ReactDOM from "react-dom";
 
-  componentDidMount() {
-    document.addEventListener("mousedown", this.handleClickOutside);
+class WatchClickOutside extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentWillMount() {
+    document.body.addEventListener("click", this.handleClick);
   }
 
   componentWillUnmount() {
-    document.removeEventListener("mousedown", this.handleClickOutside);
+    // remember to remove all events to avoid memory leaks
+    document.body.removeEventListener("click", this.handleClick);
+  }
+  myRef = React.createRef();
+  handleClick(event) {
+    const { onClickOutside } = this.props;
+
+    // if there is no proper callback - no point of checking
+    if (typeof onClickOutside !== "function") {
+      return;
+    }
+
+    // if target is container - container was not clicked outside
+    // if container contains clicked target - click was not outside of it
+    if (!this.myRef.current.contains(event.target)) {
+      onClickOutside(event); // clicked outside - fire callback
+    }
   }
 
-  myRef = React.createRef();
+  render() {
+    return <div ref={this.myRef}>{this.props.children}</div>;
+  }
+}
 
-  handleClickOutside = e => {
-    if (!this.myRef.current.contains(e.target)) {
-      this.setState({ clickedOutside: true });
-    }
-  };
-
-  handleClickInside = () => this.setState({ clickedOutside: false });
+class Demo extends React.Component {
+  handleClickOutside() {
+    alert("clicked outside!");
+  }
 
   render() {
     return (
-      <button ref={this.myRef} onClick={this.handleClickInside}>
-        {this.state.clickedOutside ? "Bye!" : "Hello!"}
-      </button>
+      <div>
+        Click on this to see alert
+        <WatchClickOutside onClickOutside={this.handleClickOutside}>
+          Click Outside of me!
+        </WatchClickOutside>
+        Click on this to see alert
+      </div>
     );
   }
 }
 
-const App = () => (
-  <div style={styles}>
-    <SampleComponent />
-  </div>
-);
-
-render(<App />, document.getElementById("root"));
-
-export default SampleComponent;
+ReactDOM.render(<Demo />, document.getElementById("root"));
